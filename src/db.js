87850a5dbf0ee4e63d4a8d1984b69c0f76448316
Julian Cipagauta <Julin Cipagauta>
2021-05-11 21:44:35 -0500

    Se añadio un catch a la conexión de mongo

--- src/db.js
+++ src/db.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose')
 
 const connect = async () => {
-  try {
-    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    console.log('Database successfully connected!')
-  } catch (e) {
-    console.error(e)
-  }
+  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Database successfully connected!'))
+    .catch((error) => console.error(error))
 }
 module.exports = connect
